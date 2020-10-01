22473687cd45f3815c1c093f4a03c6e84a350da7
Roei Berkovich <roei.berkovich@appsflyer.com>
2020-10-01 16:29:03 +0300

    add-env-files

--- src/App.js
+++ src/App.js
@@ -3,10 +3,12 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  console.log("Roei",process.env)
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
+          <h2>{process.env.REACT_APP_MYVAR}</h2>
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
